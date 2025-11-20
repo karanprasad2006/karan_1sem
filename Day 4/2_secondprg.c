1ab5d9b751f879af2b521d5372eeaf455d23c4b6
Sanjuk526392 <sanjukadapatti36@gmail.com>
2025-11-20 15:12:02 +0530

    day  4 code pushed

---
+++ Day 4/2_secondprg.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf(" no mercy for cowards!!");
+    return 0;
+    
+}
---
+++ Day 4/3_datatype.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+   int a =15;
+   char name = 'b';
+   float f= 5.9;
+   bool dancing = true;
+   printf("the age is-%d \n",a);
+    printf("the name is - %c\n",name);
+    printf("the heigh is- %f \n",f);
+
+printf("Dancing-%d \n",dancing);
+
+   return 0;
+}
